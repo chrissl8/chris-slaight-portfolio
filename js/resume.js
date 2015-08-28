1699de2a97b35886951cedcfde781c58bab47273
chrissl8 <chrisjslaight@gmail.com>
2015-08-28 12:27:46 -0400

    Add fade toggle functionality test

--- js/resume.js
+++ js/resume.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
 	$('#collapse-list').mousedown(function() {
 		$('.resume-list').hide();
-	});
+	})
 
 	$('#expand-list').mousedown(function() {
 		$('.resume-list').show();
-	});
+	})
 
+	$('#fade-toggle').click(function () {
+  		$('.resume-list').fadeToggle(1000);
+  	})	
 
 });
