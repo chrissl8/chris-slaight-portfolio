27fb4d4c756812cd6c67da07d8926050f7b98d80
chrissl8 <chrisjslaight@gmail.com>
2015-08-27 21:56:30 -0400

    Add jQuery resume list test for mobile testing

---
+++ js/resume.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$('#collapse-list').mousedown(function() {
+		$('.resume-list').hide();
+	});
+
+	$('#expand-list').mousedown(function() {
+		$('.resume-list').show();
+	});
+
+
+});
